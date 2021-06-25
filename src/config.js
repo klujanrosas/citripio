e422226cb0256553a1ef00ac7569e8ea7ec4fed7
Kenneth Lujan <kenneth.lujan@kw.com>
2021-06-25 11:18:58 -0500

    test: fix logic for TIMESTAP

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export function getConfig() {
   return {
-    TIMESTAMP: Boolean(process.env.TIMESTAMP),
+    TIMESTAMP: process.env.TIMESTAMP === 'true',
     PORT: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
     LOGGING: Boolean(process.env.LOGGING),
   };
